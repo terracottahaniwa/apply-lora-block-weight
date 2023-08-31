c5d60bdb8a652a58d19512618e85828f09e79d0a
terracottahaniwa <terracottahaniwa@gmail.com>
2023-09-01 06:07:25 +0900

    rewrite

--- apply_block_weight.py
+++ apply_block_weight.py
@@ -19,6 +19,8 @@ def main():
     LOAD_PATH = args.input
     SAVE_PATH = args.output
     RATIOS = [float(x) for x in args.ratios.split(",")]
+    LAYERS = len(RATIOS)
+    assert LAYERS in [17, 26]
 
     BLOCKID17 = [
         "BASE", "IN01", "IN02", "IN04", "IN05", "IN07", "IN08", "M00",
@@ -27,37 +29,37 @@ def main():
         "BASE", "IN00", "IN01", "IN02", "IN03", "IN04", "IN05", "IN06", "IN07", "IN08", "IN09", "IN10", "IN11", "M00",
         "OUT00", "OUT01", "OUT02", "OUT03", "OUT04", "OUT05", "OUT06", "OUT07", "OUT08", "OUT09", "OUT10", "OUT11"]
 
-    LAYERS = len(RATIOS)
-    assert LAYERS in [17, 26]
-
     if LAYERS == 17:
         RATIO_OF_ = dict(zip(BLOCKID17, RATIOS))
     if LAYERS == 26:
         RATIO_OF_ = dict(zip(BLOCKID26, RATIOS))
     print(RATIO_OF_)
 
+    PATTERNS = [
+        r"^transformer_text_model_(encoder)_layers_(\d+)_.*",
+        r"^diffusion_model_(in)put_blocks_(\d+)_.*",
+        r"^diffusion_model_(middle)_block_(\d+)_.*",
+        r"^diffusion_model_(out)put_blocks_(\d+)_.*"]
+
+    def replacement(match):
+        g1 = str(match.group(1))  # encoder, in, middle, out
+        g2 = int(match.group(2))  # number
+        assert g1 in ["encoder", "in", "middle", "out"]
+        assert isinstance(g2, int)
+
+        if g1 == "encoder":
+            return "BASE"
+        if g1 == "middle":
+            return "M00"
+        return f"{str.upper(g1)}{g2:02}"
+
     def compvis_name_to_blockid(compvis_name):
-        PATTERNS = [
-            r"^transformer_text_model_(encoder)_layers_(\d+)_.*",
-            r"^diffusion_model_(in)put_blocks_(\d+)_.*",
-            r"^diffusion_model_(middle)_block_(\d+)_.*",
-            r"^diffusion_model_(out)put_blocks_(\d+)_.*"]
-
-        def replacement(match):
-            g1 = str(match.group(1))  # encoder, in, middle, out
-            g2 = int(match.group(2))  # number
-            assert g1 in ["encoder", "in", "middle", "out"]
-            assert isinstance(g2, int)
-
-            if g1 == "encoder":
-                return "BASE"
-            if g1 == "middle":
-                return "M00"
-            return f"{str.upper(g1)}{g2:02}"
-
         strings = compvis_name
         for pattern in PATTERNS:
             strings = re.sub(pattern, replacement, strings)
+            if strings != compvis_name:
+                break
+        assert strings != compvis_name
         blockid = strings
 
         if LAYERS == 17:
@@ -69,7 +71,7 @@ def main():
     with safe_open(LOAD_PATH, framework="pt", device="cpu") as f:
         tensors = {}
         for key in f.keys():
-            tensors[key] = f.get_tensor(key)
+            tensors[key] = f.get_tensor(key)  # key = diffusers_name
             compvis_name = convert_diffusers_name_to_compvis(key, is_sd2=False)
             blockid = compvis_name_to_blockid(compvis_name)
             if compvis_name.endswith("lora_up.weight"):
